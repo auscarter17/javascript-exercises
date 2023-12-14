abca3d0fd7edfabf01045f337e9145eaba4b2755
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2023-12-14 15:53:00 -0500

    solved problem 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR"
+    } else if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;   
+    }
+    for (let step = num1; step <= num2; step++) {
+        finalSum = (finalSum + step);
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
